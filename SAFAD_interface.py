35c096bb340e3fea7234671e2f0125845c56f724
Johan Karlsson <johan.o.karlsson@slu.se>
2025-02-19 20:14:39 +0100

    Bulk commit

--- SAFAD_interface.py
+++ SAFAD_interface.py
@@ -15,69 +15,112 @@ class SAFAD_interface(object):
         path_recipies = os.path.join(self.data_path,'SAFAD IP Recipes.csv')
         path_orig = os.path.join(self.data_path,'SAFAD IP Origin and Waste of RPC SE.csv')
         path_footprints = os.path.join(self.data_path,'SAFAD ID Footprints RPC.csv')
-
-        SAFAD_recipies = pd.read_csv(path_recipies, index_col='Food code').rename_axis('code')
-        SAFAD_recipies.index = SAFAD_recipies.index.str.strip()
-
-        SAFAD_orig = pd.read_csv(path_orig,index_col='RPC Code').rename_axis('code')
-        SAFAD_orig.index = SAFAD_orig.index.str.strip()
-        
-        SAFAD_footprints = pd.read_csv(path_footprints, index_col=['Long code', 'Country code']).rename_axis(index=['code','country code'])
-        SAFAD_footprints.index = SAFAD_footprints.index.set_levels([level.str.strip() for level in SAFAD_footprints.index.levels])
+        path_transport_EF = os.path.join(self.data_path,'SAFAD IEF Transport.csv')
 
         self.raw_data = {
-            'recipies' : SAFAD_recipies,
-            'orig' : SAFAD_orig,
-            'footprints' : SAFAD_footprints
+            'recipies' : None,
+            'orig' : None,
+            'footprints' : None,
+            'transport_EF' : None
         }
 
-    def get_footprints(self, code):
+        self.read_data('recipies',path_recipies)
+        self.read_data('orig',path_orig)
+        self.read_data('footprints',path_footprints)
+        self.read_data('transport_EF',path_transport_EF)
+
+    def read_data(self, table, path):
+
+        if table == 'recipies':
+            df = _read_SAFAD_csv(
+                path = path,
+                index = {'Food code':'code'}
+            )
+        elif table == 'orig':
+            df = _read_SAFAD_csv(
+                path = path,
+                index = {'RPC Code':'code'}
+            )
+        elif table == 'footprints':
+            df = _read_SAFAD_csv(
+                path = path,
+                index = {
+                    'Long code':'code',
+                    'Country code':'country code'
+                }
+            )
+        elif table == 'transport_EF':
+            df = _read_SAFAD_csv(
+                path = path,
+                index = {
+                    'Importing, code':'import country code',
+                    'Exported from, code':'country code'
+                },
+                cols = {
+                    'Carbon dioxide':'Transport (CO2)',
+                    'Methane, fossil':'Transport (CH4, fossil)',
+                    'Nitrous oxide':'Transport (N2O)'
+                }
+            )
+
+        self.raw_data.update({table : df})
+
+    def get_footprints(self, code, use_waste_factor=False, importing_country='SE'):
         
         # Get data tables
-        SAFAD_recipies, SAFAD_orig, SAFAD_footprints = self.raw_data.values()
+        recipies, orig, footprints, transport_EF = self.raw_data.values()
 
         RPC_final = pd.Series(index=pd.Index([], name='code'), dtype=float)
-        RPC = SAFAD_recipies.loc[[code]].set_index('Component code').rename_axis('code')
+        RPC = recipies.loc[[code]].set_index('Component code').rename_axis('code')
         
         while len(RPC) > 0:
             if isinstance(RPC, pd.Series):
-                RPC = pd.merge(RPC, SAFAD_recipies.loc[RPC.index], left_index=True, right_index=True).set_index('Component code').rename_axis('code')
+                RPC = pd.merge(RPC, recipies.loc[RPC.index], left_index=True, right_index=True).set_index('Component code').rename_axis('code')
             if 'factor' in RPC.columns:
                 RPC = (RPC['factor'] * (RPC['Percentage']/100) * RPC['Reverse Yield Factor'] * RPC['Allocation Factor']).rename('factor')
             else:
                 RPC = ((RPC['Percentage']/100) * RPC['Reverse Yield Factor'] * RPC['Allocation Factor']).rename('factor')
             
-            RPC_to_final = RPC.loc[[code in SAFAD_orig.index for code in RPC.index]]
-            RPC = RPC.loc[[code not in SAFAD_orig.index for code in RPC.index]]
+            RPC_to_final = RPC.loc[[code in orig.index for code in RPC.index]]
+            RPC = RPC.loc[[code not in orig.index for code in RPC.index]]
             RPC_final = pd.concat([RPC_final, RPC_to_final]).rename('factor')
         
         RPC_orig = (
-            pd.merge(RPC_final, SAFAD_orig, left_index=True, right_index=True)
+            pd.merge(RPC_final, orig, left_index=True, right_index=True)
             .set_index('Producer Country Code', append=True)
             .rename_axis(index=['code', 'country code'])
         )
         
         # Drop country code "SE" (Sweden) as we are only interested in imports
-        RPC_orig = RPC_orig.drop('SE', level='country code', errors='ignore')
+        RPC_orig = RPC_orig.drop(importing_country, level='country code', errors='ignore')
         # Calculate import sahres
         RPC_orig['Import Share'] = RPC_orig['Share'].groupby('code').apply(lambda x: x/x.sum()).droplevel(0)
         
-        RPC_footprint = pd.merge(RPC_orig, SAFAD_footprints, left_index=True, right_index=True)
+        RPC_footprint = pd.merge(RPC_orig, footprints, left_index=True, right_index=True)
+
+        # Add in transport emissions
+        RPC_footprint = pd.merge(RPC_footprint, transport_EF.loc[importing_country], left_index=True, right_index=True)
         
         RPC_footprint['Import Share w. data'] = RPC_footprint['Import Share'].groupby('code').apply(lambda x: x/x.sum()).droplevel(0)
 
         if (RPC_footprint['Import Share'].groupby('code').sum() != 1).any():
             pass
             # Handle missing data for some import countries
+
+        # Add country as index
+        RPC_footprint.set_index('Producer Country Name', append=True, inplace=True)
         
         final_footprint = (
             RPC_footprint
-            .loc[:,'Carbon footprint, primary production':'Manure management (N2O)']
-            .mul(
-                (RPC_footprint['factor'] * (1/(1 - RPC_footprint['Waste'])) * RPC_footprint['Import Share w. data']),
+            .loc[:,'Carbon footprint, primary production':'Transport (N2O)']
+            .mul((
+                    RPC_footprint['factor'] *
+                    ((1/(1 - RPC_footprint['Waste'])) if use_waste_factor else 1) *
+                    RPC_footprint['Import Share w. data']
+                ),
                 axis=0
             )
-            .sum()
+            .groupby('Producer Country Name').sum().unstack().rename_axis(['footprint','country'])
         )
     
         return final_footprint
@@ -85,27 +128,19 @@ class SAFAD_interface(object):
     def get_GHG(
         self,
         code,
-        processes = ['Mineral fertiliser production',
-                    'Capital goods',
-                    'Energy primary production',
-                    'Land use change',
-                    'Enteric fermentation',
-                    'Manure management'],
         CO2eq = 'GWP100 AR4'
         ):
-
-        # THERE SEEM TO BE SOME ERRORS IN THE DATABASE
-        # SO THIS DOES NOT PRODUCE RELIABLE RESULTS!!!
-
         
-        SAFAD_footprints_to_process_and_GHG = {
+        footprints_to_process_and_GHG = {
             'Mineral fertiliser production (CO2)' : ('Mineral fertiliser production', 'CO2'),
             'Capital goods (CO2)' : ('Capital goods', 'CO2'),
             'Energy primary production (CO2)' : ('Energy primary production', 'CO2'),
             'Land use change (CO2)' : ('Land use change', 'CO2'),
+            'Transport (CO2)' : ('Transport', 'CO2'),
             'Mineral fertiliser production (CH4, fossil)' : ('Mineral fertiliser production', 'CH4fos'),
             'Capital goods (CH4, fossil)' : ('Capital goods', 'CH4fos'),
             'Energy primary production (CH4, fossil)' : ('Energy primary production', 'CH4fos'),
+            'Transport (CH4, fossil)' : ('Transport', 'CH4fos'),
             'Soil emissions (CH4, biogenic)' : ('Soil emissions', 'CH4bio'),
             'Enteric fermentation (CH4, biogenic)' : ('Enteric fermentation', 'CH4bio'),
             'Manure management (CH4, biogenic)' : ('Manure management', 'CH4bio'),
@@ -113,49 +148,29 @@ class SAFAD_interface(object):
             'Capital goods (N2O)' : ('Capital goods', 'N2O'),
             'Soil emissions (N2O)' : ('Soil emissions', 'N2O'),
             'Energy primary production (N2O)' : ('Energy primary production', 'N2O'),
-            'Manure management (N2O)' : ('Manure management', 'N2O')
+            'Manure management (N2O)' : ('Manure management', 'N2O'),
+            'Transport (N2O)' : ('Transport', 'N2O')
         }
 
-        GHG_emissions = self.get_footprints(code).loc[SAFAD_footprints_to_process_and_GHG.keys()]
+        GHG_emissions = self.get_footprints(code).loc[footprints_to_process_and_GHG.keys()]
         GHG_emissions.index = pd.MultiIndex.from_tuples(
-            [SAFAD_footprints_to_process_and_GHG[i] for i in GHG_emissions.index],
-            names = ['process', 'compound']
+            [footprints_to_process_and_GHG[f]+(c,) for f,c in GHG_emissions.index],
+            names = ['process', 'compound', 'country']
         )
-        GHG_emissions = GHG_emissions.loc[processes].groupby('compound').sum()
 
         if CO2eq:
             to_CO2eq = _get_CO2eq_dict(CO2eq)
 
-            GHG_emissions = GHG_emissions * [to_CO2eq[cp] for cp in GHG_emissions.index]
+            GHG_emissions = GHG_emissions * [to_CO2eq[cp] for cp in GHG_emissions.index.get_level_values('compound')]
 
         return GHG_emissions
 
-    def get_CO2e(
-        self,
-        code,
-        processes = ['Mineral fertiliser production',
-                    'Capital goods',
-                    'Energy primary production',
-                    'Land use change',
-                    'Enteric fermentation',
-                    'Manure management']
-    ):
-        
-        SAFAD_footprints_to_process_CO2e = {
-            'Mineral fertiliser production (CO2e)' : 'Mineral fertiliser production',
-            'Capital goods (CO2e)' : 'Capital goods',
-            'Energy primary production (CO2e)' : 'Energy primary production',
-            'Land use change (CO2e)' : 'Land use change',
-            'Soil emissions (CO2e)' : 'Soil emissions',
-            'Enteric fermentation (CO2e)' : 'Enteric fermentation',
-            'Manure management (CO2e)' : 'Manure management',
-        }
-        
-        GHG_emissions = self.get_footprints(code).loc[SAFAD_footprints_to_process_CO2e.keys()]
-        GHG_emissions.index = pd.Index(
-            [SAFAD_footprints_to_process_CO2e[i] for i in GHG_emissions.index],
-            name = 'process'
-        )
-        GHG_emissions = GHG_emissions.loc[processes].sum()
-
-        return GHG_emissions
+def _read_SAFAD_csv(path, index:dict, cols:None|dict=None):
+    df = pd.read_csv(path, index_col=list(index.keys())).rename_axis(list(index.values()))
+    if len(index) > 1:
+        df.index = df.index.set_levels([level.str.strip() for level in df.index.levels])
+    else:
+        df.index = df.index.str.strip()
+    if cols:
+        df = df.loc[:,list(cols.keys())].rename(cols, axis=1)
+    return df
---
+++ runCIBUSmod.py
@@ -0,0 +1,287 @@
+import sys
+import os
+import time
+
+from contextlib import redirect_stdout, redirect_stderr
+
+sys.path.insert(0, os.path.join(os.getcwd(),'../CIBUSmod'))
+import CIBUSmod as cm
+
+def do_run(
+        session : cm.Session,
+        scn_year : tuple,
+        geodist_kwargs : dict = {}
+        ):
+
+    # Activate session in environment
+    session.activate()
+
+    scn, year = scn_year
+
+    # Create log folder if it does not exist
+    log_path = os.path.join(session.data_path_output, 'log')
+    if not os.path.exists(log_path):
+        os.makedirs(log_path)
+
+    with open(os.path.join(log_path, f'{scn}_{year}.log'), 'w') as f,\
+        redirect_stdout(f), redirect_stderr(f):
+
+        print(session.data_path)
+
+        tic = time.time()
+    
+        # Increase timeout to avoid failing to write if multiple processes try to write at the same time
+        session.db_timeout = 60
+    
+        ###############################
+        ###   INSTANTIATE MODULES   ###
+        ###############################
+            
+        # Instatiate Regions
+        regions = cm.Regions(
+            par = cm.ParameterRetriever('Regions'),
+            settings = {
+                'max_land_use_from_scenario_x0' : True
+            }
+        )
+        
+        # Instantiate DemandAndConversions
+        demand = cm.DemandAndConversions(
+            par = cm.ParameterRetriever('DemandAndConversions')
+        )
+        
+        # Instantiate CropProduction
+        crops = cm.CropProduction(
+            par = cm.ParameterRetriever('CropProduction'),
+            regions = regions
+        )      
+        
+        # Instantiate AnimalHerds
+        # Each AnimalHerd object is stored in an indexed pandas.Series
+        herds = cm.make_herds(regions)
+        
+        # Instantiate WasteAndCircularity
+        waste = cm.WasteAndCircularity(
+            demand = demand,
+            crops = crops,
+            herds = herds,
+            par = cm.ParameterRetriever('WasteAndCircularity')
+        )
+        
+        # Instantiate feed management
+        feed_mgmt = cm.FeedMgmt(
+            herds = herds,
+            par = cm.ParameterRetriever('FeedMgmt')
+        )
+        
+        # Instantiate by-product management
+        byprod_mgmt = cm.ByProductMgmt(
+            demand = demand,
+            herds = herds,
+            par = cm.ParameterRetriever('ByProductMgmt')
+        )
+        
+        # Instantiate manure management
+        manure_mgmt = cm.ManureMgmt(
+            herds = herds,
+            feed_mgmt = feed_mgmt,
+            par = cm.ParameterRetriever('ManureMgmt'),
+            settings = {
+                'NPK_excretion_from_balance' : True
+            }
+        )
+        
+        # Instantiate crop residue managment
+        crop_residue_mgmt = cm.CropResidueMgmt(
+            demand = demand,
+            crops = crops,
+            herds = herds,
+            par = cm.ParameterRetriever('CropResidueMgmt')
+        )
+
+        # Instantiate cover crops management
+        cover_crops_mgmt = cm.CoverCropsMgmt(
+            crops = crops,
+            par = cm.ParameterRetriever('CoverCropsMgmt')
+        )
+        
+        # Instantiate plant nutrient management
+        plant_nutrient_mgmt = cm.PlantNutrientMgmt(
+            demand = demand,
+            regions = regions,
+            crops = crops,
+            waste = waste,
+            herds = herds,
+            cover_crops_mgmt = cover_crops_mgmt,
+            par = cm.ParameterRetriever('PlantNutrientMgmt')
+        )
+        
+        # Instatiate machinery and energy management
+        machinery_and_energy_mgmt  = cm.MachineryAndEnergyMgmt(
+            regions = regions,
+            crops = crops,
+            waste = waste,
+            herds = herds,
+            par = cm.ParameterRetriever('MachineryAndEnergyMgmt')
+        )
+        
+        # Instatiate inputs management
+        inputs = cm.InputsMgmt(
+            demand = demand,
+            crops = crops,
+            waste = waste,
+            herds = herds,
+            par = cm.ParameterRetriever('InputsMgmt')
+        )
+        
+        # Instantiate geo distributor
+        geodist = cm.GeoDistributor(
+            regions = regions,
+            demand = demand,
+            crops = crops,
+            herds = herds,
+            feed_mgmt = feed_mgmt,
+            par = cm.ParameterRetriever('GeoDistributor')
+        )
+    
+        ############################
+        ###   RUN CALCULATIONS   ###
+        ############################
+    
+        # Update all parameter values
+        cm.ParameterRetriever.update_all_parameter_values(
+            **session[scn],
+            year = year
+        )
+
+        if year != '0':
+            while True:
+                try:
+                    # Get animal numbers in baseline
+                    bl_ani = session.get_attr('geo','x_ani', scn='SLV', years='0').iloc[0]
+                    bl_crp = session.get_attr('geo','x_crops', scn='SLV', years='0').iloc[0]
+                except:
+                    time.sleep(10)
+                else:
+                    break
+        
+        # Get region attributes
+        regions.calculate(verbose=True)
+
+        if year != '0':
+            # Set x0 to baseline animal numbers and ares
+            regions.data_attr.get('x0_animals').loc[:] = \
+                bl_ani.groupby(['species','breed','prod_system','region']).sum()
+            regions.data_attr.get('x0_crops').loc[:] = \
+                bl_crp.reindex(regions.data_attr.get('x0_crops').index, fill_value=0)
+        
+        # Calculate food demand
+        demand.calculate(verbose=True)
+        
+        # Calculate crops
+        crops.calculate(verbose=True)
+        
+        # Calculate herds
+        for h in herds:
+            h.calculate(verbose=True)
+    
+        # Induce beef exports in DemandAndConversions if beef production from dairy
+        # systems under given demand for milk products exceeds total beef demand.
+        # This is to avoid not finding any solution when running the GeoDistributor.
+        cm.helpers.induce_beef_exports(
+            demand = demand,
+            herds = herds
+        )
+        
+        # Calculate feed
+        feed_mgmt.calculate(verbose=True)    
+        
+        # Distribute animals and crops
+        # Make optimisation problem
+        kwargs = {
+            'use_cons' : [1,2,3,4,5,6,7],
+            'verbose' : True
+        }
+
+        kwargs.update(geodist_kwargs)
+
+        if year != '0':
+            C8_ani = []
+            C8_crp = []
+            C8_rel = []
+            
+            # Constrain "other sheep" to baseline numbers
+            C8_ani.append(bl_ani.xs(('sheep','other sheep'), level=('species', 'sub_system'), drop_level=False))
+            C8_crp.append(None)
+            C8_rel.append('==')
+
+            # Constrain semi-natural meadows and pastures with thin soils and wooded pastures to baseline areas
+            C8_ani.append(None)
+            C8_crp.append(bl_crp.loc[['Semi-natural meadows', 'Semi-natural pastures, thin soils', 'Semi-natural pastures, wooded']])
+            C8_rel.append('==')
+
+            # Constrain Fallow and Ley not harvested to 50% of baseline areas
+            C8_ani.append(None)
+            C8_crp.append(bl_crp.loc[['Fallow', 'Ley not harvested']] * 0.5)
+            C8_rel.append('>=')
+            
+            kwargs.update({
+                'use_cons' : [1,2,3,4,5,6,7,8],
+                'C8_ani' : C8_ani,
+                'C8_crp' : C8_crp,
+                'C8_rel' : C8_rel
+            })
+
+        geodist.make(**kwargs)
+        # Solve optimisation problem
+        geodist.solve(verbose=True)
+        
+        # Redistribute feeds (not yet implemented) and calculate enteric CH4 emissions
+        feed_mgmt.calculate2(verbose=True)
+    
+        # Balance by-product demand and suply
+        byprod_mgmt.calculate(verbose=True)
+        
+        # Calculate manure
+        manure_mgmt.calculate(verbose=True)
+        
+        # Calculate harvest of crop residues
+        crop_residue_mgmt.calculate(verbose=True)
+
+        # Calculate cover crop areas
+        cover_crops_mgmt.calculate(verbose=True)
+    
+        # Calculate treatment of wastes and other feedstocks
+        waste.calculate(verbose=True)
+        
+        # Calculate plant nutrient management
+        plant_nutrient_mgmt.calculate(verbose=True)
+        
+        # Calculate energy requirements
+        machinery_and_energy_mgmt.calculate(verbose=True)
+        
+        # Calculate inputs supply chain emissions
+        inputs.calculate(verbose=True)
+    
+        ########################
+        ###   STORE OUTPUT   ###
+        ########################
+        
+        # Store results (try again if first atempt fails)
+        try:
+            session.store(
+                scn, year,
+                demand, regions, crops, waste, herds, geodist
+            )
+        except:
+            session.store(
+                scn, year,
+                demand, regions, crops, waste, herds, geodist
+            )
+    
+        t = time.time() - tic
+        m = int(t/60)
+        s = int(round(t - m*60))
+        print(f'{scn}, {year} finished successfully in {m}min {s}s')
+
+    return t
